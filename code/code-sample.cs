5bcee1954e383f7509e3bf5575a68d333e33900f
david7ce <david7ce@outlook.com>
2023-01-17 22:43:44 +0000

    add

---
+++ code/code-sample.cs
@@ -0,0 +1,9 @@
+var names = new List<string>() { "John", "Tom", "Peter" };
+foreach (string name in names)
+{
+    if (name == "Tom")
+    {
+        continue;
+    }
+    Console.WriteLine(name);
+}
